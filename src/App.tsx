11a90d1db6eab41b2a1b7a570b826a3050aafca5
Perennial <gopimudumal99@gmail.com>
2022-06-28 14:52:44 +0530

    todo input created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
+import InputFeild from './components/InputFeild';
+import { Todo } from './model';
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("")
+  const [todos,setTodos] = useState<Todo[]>([])
+
+  
+  const handleAdd = (e:React.FormEvent) =>{
+      e.preventDefault();
+      if(todo){
+        setTodos([...todos,{id:Date.now(),todo,isDone:false}])
+        setTodo("")
+      }
+  }
+  
   return (
     <div className="App">
-    Hello world
+    <span className='heading'> Taskify </span>
+    <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+    {todos.map((t)=><li>{t.todo}</li>)}
     </div>
   );
 }
